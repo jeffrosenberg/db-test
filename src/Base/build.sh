049d9563b4f5d052752f7173df7d5327ab6da87c
Jeff Rosenberg <rosenberg.jeff@gmail.com>
2022-01-24 20:59:47 -0600

    Upgrade to Sdk version 1.17.3

--- src/Base/build.sh
+++ src/Base/build.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT_NAME="${1:-Base}"
 BUILD_CONFIGURATION=${2:-"Debug"}
-VERSION=${3:-"7.6.0"}
+VERSION=${3:-"1.1.0"}
 PROJECT_DIR="${4:-${SCRIPT_DIR}}"
 PROJECT_FILE="${PROJECT_DIR}/${PROJECT_NAME}.csproj"
 NUGET_SRC=${NUGET_SRC:-"${HOME}/.nuget/packages/"}
@@ -18,4 +18,4 @@ target="${NUGET_SRC}/${PROJECT_NAME}/${version}" && \
   rm -rf ${target}
 
 # Push the package to the local repo
-dotnet nuget push "${PROJECT_DIR}/bin/${BUILD_CONFIGURATION}/${PROJECT_NAME}.${VERSION}.nupkg" --source ${NUGET_SRC}
+dotnet nuget push "${PROJECT_DIR}/bin/${BUILD_CONFIGURATION}/${PROJECT_NAME}*.nupkg" --source ${NUGET_SRC}
